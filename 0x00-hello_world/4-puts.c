102f1276a337c040ac738756aa65cbd386f7a7b0
Mariam Banire <mariambanire@gmail.com>
2022-04-08 00:43:31 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 *main - function begins
-Return: function ends
+*
+*Return: function ends
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
