f22a685fb0a32f0aac15ac4d5c39f3b218bfce58
Mariam Banire <mariambanire@gmail.com>
2022-04-13 13:39:06 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
-*print_alphabet - Entry point
-*Return:0
+* print_alphabet - print alphabet lowercase
+*
+* Return: Always 0.
 */
+
 void print_alphabet(void)
 {
-	char letter = 'a';
+char alphabet;
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n')
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
 }
