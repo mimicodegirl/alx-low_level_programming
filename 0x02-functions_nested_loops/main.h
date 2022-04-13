cd20579a94ac4b20d1c07692104f669d86b80c78
Mariam Banire <mariambanire@gmail.com>
2022-04-13 02:55:53 -0700

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-
 int_putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,5 +11,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
