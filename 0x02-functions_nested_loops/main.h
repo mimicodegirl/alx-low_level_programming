3d775376e72c4f67594aa1807a991155a61f3b62
Mariam Banire <mariambanire@gmail.com>
2022-04-13 04:31:24 -0700

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,14 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-int_putchar(char c);
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int_n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
