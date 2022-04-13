feb576cc12bd27cd92af207dba5b80a2bc085f98
Mariam Banire <mariambanire@gmail.com>
2022-04-13 04:23:00 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include "main.h"
-
 /**
-*main - prints _putchar
-*
-*Description: prints _putchar
-*
-Return: 0 (Success)
+*main-main block
+*Description:print -putchar in a new line
+*Return:this program returns zero
 */
-
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
